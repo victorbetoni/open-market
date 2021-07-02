38f4d4dc527fa6426f1f4ddfe51e187f9ca030fc
threader <40972803+localthreader@users.noreply.github.com>
2021-07-02 15:21:50 -0300

    Removing unused method

--- src/main/scala/net/threader/openmarket/model/MarketItem.scala
+++ src/main/scala/net/threader/openmarket/model/MarketItem.scala
@@ -6,6 +6,4 @@ import org.bukkit.inventory.ItemStack
 import java.time.LocalDateTime
 import java.util.UUID
 
-case class MarketItem(holder: UUID, id: UUID, item: ItemStack, expireAt: LocalDateTime) {
-  def serializeItemStack(): String = new Gson().toJson(item.serialize())
-}
+case class MarketItem(holder: UUID, id: UUID, item: ItemStack, expireAt: LocalDateTime)
